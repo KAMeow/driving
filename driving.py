a2bad7f15292a15d36d8711609038ed9ff876a89
Katrina <katrinayu89@gmail.com>
2020-06-10 15:56:01 +0800

    update

--- driving.py
+++ driving.py
@@ -1,9 +1,15 @@
 country = input('please enter your country:')
 age = input('please enter your age:')
+age = int(age)
 if country == 'Taiwan':
 	if age >= 18:
 		print('you are able to take driver license')
 	else:
 		print('you are unable to take driver license')
+elif country == 'American':
+	if age >= 16:
+		print('you are able to take driver license')
+	else:
+		print('you are unable to take driver license')
 else:
-	print('sorry, we cannot serve you.')
+	print('sorry, this is only for Taiwan / American')
